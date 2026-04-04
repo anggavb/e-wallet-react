2a2f6115192590daaeb365f3f2889ee635db5e48
anggavb <anggavb8@gmail.com>
2026-04-05 06:45:20 +0700

    chore(vite.config): add resolve alias for easier imports

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@components': '/src/components',
+      '@layouts': '/src/components/layouts',
+      '@hooks': '/src/hooks',
+      '@pages': '/src/pages',
+    },
+  },
 })
