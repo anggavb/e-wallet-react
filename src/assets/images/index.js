9be89993b8dd34a82da2fbdf2feb8cce086a4f6e
anggavb <anggavb8@gmail.com>
2026-04-06 21:25:39 +0700

    feat(images): add new image assets and update index export

---
+++ src/assets/images/index.js
@@ -0,0 +1,23 @@
+import adisurya from './adisurya.png'
+import buzjany from './buzjany.png'
+import profile from './Ellipse 185.png'
+import floydMiles from './floyd-miles.png'
+import miguelle from './miguelle.png'
+import raulemons from './raulemons.png'
+import reiva from './reiva.png'
+import robertFox from './robert-fox.png'
+import thobie from './thobie.png'
+import ujang from './ujang.png'
+
+export {
+  adisurya,
+  buzjany,
+  profile,
+  floydMiles,
+  miguelle,
+  reiva,
+  robertFox,
+  raulemons,
+  thobie,
+  ujang
+}
