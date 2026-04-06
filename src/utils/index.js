5e5c706ef94fecd9092cee058f85acf60815b936
anggavb <anggavb8@gmail.com>
2026-04-07 05:06:56 +0700

    feat(transaction): add listHistoryTransactions utility for transaction data

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,9 @@
 import listMenus from './listMenus';
 import listWidget from './listWidget';
+import listHistoryTransactions from './listHistoryTransactions';
 
 export {
   listMenus,
   listWidget,
+  listHistoryTransactions
 };
---
+++ src/utils/listHistoryTransactions.js
@@ -0,0 +1,67 @@
+import { adisurya, buzjany, floydMiles, miguelle, reiva, robertFox, raulemons, thobie, ujang } from '@/assets/images';
+
+export default [
+  {
+    photo: robertFox,
+    name: 'Robert Fox',
+    amount: 'Rp 50.000',
+    isTransfer: true,
+    phone: '082116304337',
+  },
+  {
+    photo: floydMiles,
+    name: 'Floyd Miles',
+    amount: 'Rp 50.000',
+    isTransfer: false,
+    phone: '(308) 555-0121',
+  },
+  {
+    photo: ujang,
+    name: 'Ujang',
+    amount: 'Rp 50.000',
+    isTransfer: false,
+    phone: '(704) 555-0127',
+  },
+  {
+    photo: raulemons,
+    name: 'Raulemons',
+    amount: 'Rp 50.000',
+    isTransfer: true,
+    phone: '(603) 555-0123',
+  },
+  {
+    photo: reiva,
+    name: 'Reiva',
+    amount: 'Rp 50.000',
+    isTransfer: true,
+    phone: '(671) 555-0110',
+  },
+  {
+    photo: thobie,
+    name: 'Thobie',
+    amount: 'Rp 50.000',
+    isTransfer: false,
+    phone: '(225) 555-0118',
+  },
+  {
+    photo: buzjany,
+    name: 'Buzjany',
+    amount: 'Rp 50.000',
+    isTransfer: true,
+    phone: '(217) 555-0113',
+  },
+  {
+    photo: adisurya,
+    name: 'Adisurya',
+    amount: 'Rp 50.000',
+    isTransfer: false,
+    phone: '(704) 555-0127',
+  },
+  {
+    photo: miguelle,
+    name: 'Miguelle',
+    amount: 'Rp 50.000',
+    isTransfer: true,
+    phone: '(308) 555-0121',
+  },
+]
