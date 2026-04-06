aa97d86a2f1e5f536b2e954c4377891cde850c41
anggavb <anggavb8@gmail.com>
2026-04-07 06:53:40 +0700

    docs(icons): update documentation for MoneyInsertIcon component

--- src/components/atoms/icons/MoneyInsertIcon.jsx
+++ src/components/atoms/icons/MoneyInsertIcon.jsx
@@ -1,3 +1,7 @@
+/**
+ * MoneyInsertIcon component that renders the SVG icon for the money insert menu item.
+ * @returns {JSX.Element}
+ */
 function MoneyInsertIcon() {
   return (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
