8997e1df4934f293969cc3e29bdf6c283485e70f
anggavb <anggavb8@gmail.com>
2026-04-07 15:33:22 +0700

    feat(listMenus): add isEnd property to menu items for improved routing

--- src/utils/listMenus.js
+++ src/utils/listMenus.js
@@ -14,30 +14,35 @@ export default [
     name: 'Dashboard',
     to: `${prefix}`,
     icon: DashboardIcon,
+    isEnd: true,
     class: 'stroke-gray-600 group-aria-[current=page]:stroke-current group-hover:stroke-blue-800',
   },
   {
     name: 'Transfer',
     to: `${prefix}/transfer`,
     icon: TransferIcon,
+    isEnd: false,
     class: 'fill-gray-600 group-aria-[current=page]:fill-current group-hover:fill-blue-800',
   },
   {
     name: 'History',
     to: `${prefix}/history`,
     icon: HistoryIcon,
+    isEnd: true,
     class: 'stroke-gray-600 group-aria-[current=page]:stroke-current group-hover:stroke-blue-800',
   },
   {
     name: 'Top Up',
     to: `${prefix}/top-up`,
     icon: TopUpIcon,
+    isEnd: true,
     class: 'fill-gray-600 group-aria-[current=page]:fill-current group-hover:fill-blue-800',
   },
   {
     name: 'Profile',
     to: `${prefix}/profile`,
     icon: ProfileIcon,
+    isEnd: true,
     class: 'stroke-gray-600 group-aria-[current=page]:stroke-current group-hover:stroke-blue-800',
   },
   {
@@ -45,6 +50,7 @@ export default [
     to: `/`,
     icon: LogoutIcon,
     onclick: handleLogout,
+    isEnd: true,
     class: 'stroke-red-600 group-hover:stroke-red-800',
   },
 ]
