b80e2f33e1506310ffeca9930b2ee8dc0b194933
anggavb <anggavb8@gmail.com>
2026-04-07 10:35:17 +0700

    feat(payment-method): add listPaymentMethod utility for payment options

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,10 +2,12 @@ import listMenus from './listMenus';
 import listWidget from './listWidget';
 import listHistoryTransactions from './listHistoryTransactions';
 import dataFinancialChart from './dataFinancialChart';
+import listPaymentMethod from './listPaymentMethod';
 
 export {
   listMenus,
   listWidget,
   listHistoryTransactions,
   dataFinancialChart,
+  listPaymentMethod
 };
---
+++ src/utils/listPaymentMethod.js
@@ -0,0 +1,23 @@
+import { BRIIcon, DanaIcon, BCAIcon, GopayIcon, OVOIcon } from "@components/atoms/icons"
+export default [
+  {
+    icon: BRIIcon,
+    name: 'Bank Rakyat Indonesia',
+  },
+  {
+    icon: DanaIcon,
+    name: 'Dana',
+  },
+  {
+    icon: BCAIcon,
+    name: 'Bank Central Asia',
+  },
+  {
+    icon: GopayIcon,
+    name: 'Gopay',
+  },
+  {
+    icon: OVOIcon,
+    name: 'OVO',
+  },
+]
