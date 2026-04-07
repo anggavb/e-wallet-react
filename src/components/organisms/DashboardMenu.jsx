8ff6a7955e7034ea1c6b4ba07269da7052482722
anggavb <anggavb8@gmail.com>
2026-04-07 14:16:17 +0700

    style(DashboardMenu): format code for consistency and improve readability

--- src/components/organisms/DashboardMenu.jsx
+++ src/components/organisms/DashboardMenu.jsx
@@ -1,5 +1,5 @@
-import { Navigation } from '@components/atoms';
-import { listMenus } from '@utils';
+import { Navigation } from "@components/atoms";
+import { listMenus } from "@utils";
 
 /**
  * DashboardMenu component that renders the navigation menu for the dashboard layout.
@@ -8,16 +8,14 @@ import { listMenus } from '@utils';
 function DashboardMenu() {
   return (
     <nav className="hidden flex-col p-8 px-3 bg-slate-50 border-r border-gray-200 md:flex lg:px-4">
-      {
-        listMenus.map((menu) => (
-          <Navigation key={menu.to} to={menu.to}>
-            {menu.icon && <menu.icon className={menu.class} />}
-            {menu.name}
-          </Navigation>
-        ))
-      }
+      {listMenus.map((menu) => (
+        <Navigation key={menu.to} to={menu.to} onClick={menu.onclick}>
+          {menu.icon && <menu.icon className={menu.class} />}
+          {menu.name}
+        </Navigation>
+      ))}
     </nav>
-  )
+  );
 }
 
-export default DashboardMenu
+export default DashboardMenu;
