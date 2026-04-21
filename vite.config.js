3157bdf09fc7d4a5d48032c8b979e9d7764fc798
anggavb <anggavb8@gmail.com>
2026-04-21 11:19:54 +0700

    feat(vite.config): add manual chunking configuration and warning documentation

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,28 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  /**
+   * Have some yellow notification:
+   
+    [plugin builtin:vite-reporter] 
+    (!) Some chunks are larger than 500 kB after minification. Consider:
+    - Using dynamic import() to code-split the application
+    - Use build.rolldownOptions.output.codeSplitting to improve chunking: https://rolldown.rs/reference/OutputOptions.codeSplitting
+    - Adjust chunk size limit for this warning via build.chunkSizeWarningLimit.
+
+    https://stackoverflow.com/questions/75719664/some-chunks-are-larger-than-500-kib-after-minification
+   */
+  build: {
+    rolldownOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
